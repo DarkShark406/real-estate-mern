5165107e1b792bd3bf9f7cd61e4a3741c040d7ac
DarkShark406 <anpl20406@st.uel.edu.vn>
2023-11-03 18:12:57 +0700

    completed delete user functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { updateUser } from "../controllers/user.controller.js";
+import { updateUser, deleteUser } from "../controllers/user.controller.js";
 import { veriryToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/update/:id", veriryToken, updateUser);
+router.delete("/delete/:id", veriryToken, deleteUser);
 
 export default router;
