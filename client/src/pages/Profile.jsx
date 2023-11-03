a9ee762edf469cac0fe6d67241de7384d6ba9ce3
DarkShark406 <anpl20406@st.uel.edu.vn>
2023-11-03 17:02:14 +0700

    complete update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,14 +7,22 @@ import {
 	uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase.js";
+import {
+	updateUserStart,
+	updateUserSuccess,
+	updateUserFailure,
+} from "../redux/user/userSlice.js";
+import { useDispatch } from "react-redux";
 
 const Profile = () => {
-	const { currentUser } = useSelector((state) => state.user);
+	const { currentUser, loading, error } = useSelector((state) => state.user);
 	const fileRef = useRef(null);
 	const [file, setFile] = useState(undefined);
 	const [filePerc, setFilePerc] = useState(0);
 	const [fileUploadError, setFileUploadError] = useState(false);
 	const [formData, setFormData] = useState({});
+	const [updateSuccess, setUpdateSuccess] = useState(false);
+	const dispatch = useDispatch();
 
 	// firebase storage
 	// 	 allow read;
@@ -55,11 +63,44 @@ const Profile = () => {
 		}
 	}, [file]);
 
+	const handleChange = (e) => {
+		console.log(formData);
+		setFormData({ ...formData, [e.target.id]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			dispatch(updateUserStart());
+			console.log(formData);
+			const res = await fetch("/api/user/update/" + currentUser._id, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+
+			const data = await res.json();
+
+			if (data.success === false) {
+				dispatch(updateUserFailure(data.message));
+				return;
+			}
+
+			dispatch(updateUserSuccess(data));
+			setUpdateSuccess(true);
+		} catch (error) {
+			dispatch(updateUserFailure(error.message));
+		}
+	};
+
 	return (
 		<div className="p-3 max-w-lg mx-auto">
 			<h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
 
-			<form className="flex flex-col gap-4">
+			<form className="flex flex-col gap-4" onSubmit={(e) => handleSubmit(e)}>
 				<input
 					type="file"
 					ref={fileRef}
@@ -93,23 +134,28 @@ const Profile = () => {
 					type="text"
 					placeholder="username"
 					id="username"
-					value={currentUser.username}
+					defaultValue={currentUser.username}
+					onChange={(e) => handleChange(e)}
 				/>
 				<input
 					className="border p-3 rounded-lg"
 					type="text"
 					placeholder="email"
 					id="email"
-					value={currentUser.email}
+					defaultValue={currentUser.email}
+					onChange={(e) => handleChange(e)}
 				/>
 				<input
 					className="border p-3 rounded-lg"
 					type="password"
 					placeholder="password"
 					id="password"
+					onChange={(e) => handleChange(e)}
 				/>
-				<button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-					Update
+				<button
+					disabled={loading}
+					className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+					{loading ? "Loading..." : "Update"}
 				</button>
 				<button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
 					Create listing
@@ -119,6 +165,12 @@ const Profile = () => {
 				<span className="text-red-700 cursor-pointer">Delete Account</span>
 				<span className="text-red-700 cursor-pointer">Sign out</span>
 			</div>
+
+			<p className="text-red-700 mt-2">{error ? error : ""}</p>
+
+			<p className="text-green-700 mt-2">
+				{updateSuccess ? "User is updated successully" : ""}
+			</p>
 		</div>
 	);
 };
