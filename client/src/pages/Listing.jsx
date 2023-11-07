d935460c62cc40ffe1ba5eee939781eec59f2a61
DarkShark406 <anpl20406@st.uel.edu.vn>
2023-11-07 20:57:16 +0700

    add loop images on swiper and add copy link functionality in Listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -11,6 +11,7 @@ import {
 	FaChair,
 	FaMapMarkedAlt,
 	FaParking,
+	FaShare,
 } from "react-icons/fa";
 import Contact from "../components/Contact";
 
@@ -61,7 +62,7 @@ const Listing = () => {
 			)}
 			{listing && !loading && !error && (
 				<>
-					<Swiper navigation>
+					<Swiper navigation loop={true}>
 						{listing.imageUrls.map((imageUrl) => (
 							<SwiperSlide key={imageUrl}>
 								<div
@@ -73,6 +74,23 @@ const Listing = () => {
 							</SwiperSlide>
 						))}
 					</Swiper>
+					<div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+						<FaShare
+							className="text-slate-500"
+							onClick={() => {
+								navigator.clipboard.writeText(window.location.href);
+								setCopied(true);
+								setTimeout(() => {
+									setCopied(false);
+								}, 2000);
+							}}
+						/>
+					</div>
+					{copied && (
+						<p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+							Link copied!
+						</p>
+					)}
 
 					<div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-3">
 						<h1 className="text-3xl font-semibold">
